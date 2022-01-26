766698baec8ffe8b2657965b86da203cfff31235
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-26 23:28:14 +0100

    Routes and action for the config of server

---
+++ routes/routes.ts
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+import { Router, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import User from '../classes/User';
+
+const router = Router()
+dotenv.config()
+
+const authenticateToken = (req: Request, res: Response, next: any) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token === null) return res.sendStatus(401)
+
+    jwt.verify(token as string, process.env.ACCESS_TOKEN_SECRET as string, (err: any, user: any) => {
+        if (err) return res.sendStatus(403)
+        req.body.user = user
+        next()
+    })
+}
+
+router.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'API - Chat App'
+    })
+})
+
+router.post('/auth', (req: Request, res: Response) => {
+    const { username, password } = req.body
+
+    const user = { name: username }
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as string, (err, token) => {
+        res.json({ accessToken: token })
+    })
+})
+
+router.get('/posts', authenticateToken, (req: Request, res: Response) => {
+    const posts = [
+        {
+            owner: 'Jose',
+            info: 'Lorem ipsum'
+        },
+        {
+            owner: 'Pepe',
+            info: 'Lorem ipsum'
+        }
+    ]
+    /* res.json(posts.filter(post => post.owner === req.body.user)) */
+    res.json(posts)
+})
+
+export default router
---
+++ socket/actions.ts
@@ -0,0 +1,28 @@
+import socketIO, { Socket } from "socket.io"
+import UsersList from '../classes/UsersList';
+
+const usersList = UsersList.instance
+
+export const connect = (client: Socket) => {
+    usersList.addUser(client.id)
+}
+
+export const disconnect = (client: Socket) => {
+    client.on('disconnect', () => {
+        console.log(` - Client disconnected`, client.id, new Date().toLocaleTimeString())
+        usersList.deleteUser(client.id)
+    })
+}
+
+export const configUser = (client: Socket) => {
+    client.on('config-user', (payload: { username: string }) => {
+        usersList.configUser(client.id, payload.username)
+    })
+}
+
+export const message = (client: Socket, io: socketIO.Server) => {
+    client.on('send-message', (payload: { message: string }) => {
+        console.log('new message', payload);
+        io.emit('new-message', payload)
+    })
+}
