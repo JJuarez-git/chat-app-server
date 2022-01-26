81a9ef9074d8c1eec68810041f73efcec8278333
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-26 23:27:19 +0100

    Initialization of WSS on index

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Test")
+import WebSocketServer from './classes/WebSocketServer'
+
+const WSServer = WebSocketServer.instance
+
+WSServer.start(() => {
+    console.log('Web Socket Server running on', WSServer.port)
+})
